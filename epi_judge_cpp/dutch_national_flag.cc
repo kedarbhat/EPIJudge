ef758422cca160e55e96c868e2b39f2712f2097e
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2019-05-14 16:39:19 -0500

    First commit

--- epi_judge_cpp/dutch_national_flag.cc
+++ epi_judge_cpp/dutch_national_flag.cc
@@ -1,18 +1,43 @@
 #include <array>
+#include <cassert>
 #include <vector>
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
-using std::vector;
-typedef enum { kRed, kWhite, kBlue } Color;
 
-void DutchFlagPartition(int pivot_index, vector<Color>* A_ptr) {
-  // TODO - you fill in here.
-  return;
+using Color = enum { kRed, kWhite, kBlue };
+
+void DutchFlagPartition(int pivot_index, std::vector<Color>* A_ptr) {
+  if (A_ptr == nullptr) {
+    return;
+  }
+
+  using diff_t = typename std::vector<Color>::difference_type;
+  auto& A = *A_ptr;
+  auto pivot_element =  A[pivot_index];
+  auto low_idx = std::size_t{0};
+  auto mid_idx = std::size_t{0};
+  auto high_idx = A.size();
+
+  while (mid_idx < high_idx) {
+    if (A[mid_idx] < pivot_element) {
+      std::swap(A[mid_idx], A[low_idx]);
+      ++low_idx;
+      ++mid_idx;
+    } else if (A[mid_idx] > pivot_element) {
+      std::swap(A[mid_idx], A[high_idx - 1]);
+      --high_idx;
+    } else {
+      ++mid_idx;
+    }
+    assert(std::all_of(std::cbegin(A), std::cbegin(A)+diff_t(low_idx), [pivot_element](auto&& i) { return i < pivot_element; }));
+    assert(std::all_of(std::cbegin(A)+diff_t(low_idx), std::cbegin(A)+diff_t(mid_idx), [pivot_element](auto&& i) { return i == pivot_element; }));
+    assert(std::all_of(std::cbegin(A)+diff_t(high_idx), std::cend(A), [pivot_element](auto&& i) { return i > pivot_element; }));
+  }
 }
-void DutchFlagPartitionWrapper(TimedExecutor& executor, const vector<int>& A,
+void DutchFlagPartitionWrapper(TimedExecutor& executor, const std::vector<int>& A,
                                int pivot_idx) {
-  vector<Color> colors;
+  std::vector<Color> colors;
   colors.resize(A.size());
   std::array<int, 3> count = {0, 0, 0};
   for (size_t i = 0; i < A.size(); i++) {
