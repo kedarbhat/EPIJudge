3ed4b2a01cebd9df1b8b223dd4bcef37d401c9da
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2019-05-14 18:39:37 -0500

    added power_x_y

--- epi_judge_cpp/power_x_y.cc
+++ epi_judge_cpp/power_x_y.cc
@@ -1,7 +1,24 @@
 #include "test_framework/generic_test.h"
 double Power(double x, int y) {
-  // TODO - you fill in here.
-  return 0.0;
+  if (std::fpclassify(x) == FP_ZERO) {
+    return 0;
+  }
+
+  auto power = int64_t(y);
+  auto result = 1.0;
+  if (power < 0) {
+    power *= -1;
+    x = 1/x;
+  }
+  while (power > 0) {
+    if (power % 2 != 0) {
+      result *= x;
+    }
+    x *= x;
+    power /= 2;
+  }
+
+  return result;
 }
 
 int main(int argc, char* argv[]) {
