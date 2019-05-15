77a897a95f2daf2515662368e1218768d32f2590
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2019-05-15 17:46:38 -0500

    added string_integer_interconversion

--- epi_judge_cpp/string_integer_interconversion.cc
+++ epi_judge_cpp/string_integer_interconversion.cc
@@ -1,16 +1,41 @@
 #include <string>
+#include <sstream>
+
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
 using std::string;
 
 string IntToString(int x) {
-  // TODO - you fill in here.
-  return "";
+  auto isNegative = std::signbit(x);
+
+  std::stringstream ss;
+  do {
+    auto c = char('0' + std::abs(x % 10));
+    ss << c;
+    x /= 10;
+  } while (x != 0);
+  if (isNegative) {
+    ss << '-';
+  }
+  auto str = ss.str();
+  return {std::crbegin(str), std::crend(str)};
 }
+
 int StringToInt(const string& s) {
-  // TODO - you fill in here.
-  return 0;
+  int result = 0;
+  int multiplier = 1;
+  for (auto c : s) {
+    assert(std::isdigit(c) || c == '-');
+    if (c == '-') {
+      multiplier *= -1;
+    } else {
+      result *= 10;
+      result += (c - '0');
+    }
+  }
+  return multiplier * result;
 }
+
 void Wrapper(int x, const string& s) {
   if (IntToString(x) != s) {
     throw TestFailure("Int to string conversion failed");
