901972d94a20a5e1274945fd323334c130939422
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2019-05-14 20:56:37 -0500

    added rotate_array

--- epi_judge_cpp/rotate_array.cc
+++ epi_judge_cpp/rotate_array.cc
@@ -3,10 +3,18 @@
 #include "test_framework/timed_executor.h"
 using std::vector;
 
-void RotateArray(int rotate_amount, vector<int>* A) {
-  // TODO - you fill in here.
-  return;
+void RotateArray(int rotate_amount, vector<int>* A_ptr) {
+  if (A_ptr == nullptr) {
+    return;
+  }
+  auto& A = *A_ptr;
+  rotate_amount %= A.size();
+
+  std::reverse(std::rbegin(A), std::rbegin(A) + rotate_amount);
+  std::reverse(std::rbegin(A) + rotate_amount, std::rend(A));
+  std::reverse(std::rbegin(A), std::rend(A));
 }
+
 vector<int> RotateArrayWrapper(TimedExecutor& executor, vector<int> A,
                                int rotate_amount) {
   executor.Run([&] { RotateArray(rotate_amount, &A); });
