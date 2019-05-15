bb8a232867ae9448e97ffe192d3a1b039b1027b7
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2019-05-15 15:20:46 -0500

    added is_tree_balanced

--- epi_judge_cpp/is_tree_balanced.cc
+++ epi_judge_cpp/is_tree_balanced.cc
@@ -1,8 +1,35 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
-bool IsBalanced(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+
+struct BalancedWithHeight {
+  int height;
+  bool isBalanced;
+};
+BalancedWithHeight TreeHeight(const unique_ptr<BinaryTreeNode<int>>& root) {
+  if (root == nullptr) {
+    return {0, true};
+  }
+  BalancedWithHeight leftHeight = TreeHeight(root->left);
+  if (leftHeight.isBalanced) {
+    auto rightHeight = TreeHeight(root->right);
+    if (rightHeight.isBalanced) {
+      return {1+std::max(leftHeight.height, rightHeight.height), std::abs(leftHeight.height-rightHeight.height) < 2};
+    }
+  }
+  return {0, false};
+}
+
+bool IsBalanced(const unique_ptr<BinaryTreeNode<int>>& root) {
+  if (root == nullptr) {
+    return true;
+  }
+
+  auto leftHeight = TreeHeight(root->left);
+  auto rightHeight = TreeHeight(root->right);
+  if (!leftHeight.isBalanced || !rightHeight.isBalanced) {
+    return false;
+  }
+  return std::abs(leftHeight.height - rightHeight.height) < 2;
 }
 
 int main(int argc, char* argv[]) {
