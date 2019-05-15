75e59a0612de98dd2c8027366b8a3ba43b310822
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2019-05-15 15:33:01 -0500

    added int_as_array_increment

--- epi_judge_cpp/int_as_array_increment.cc
+++ epi_judge_cpp/int_as_array_increment.cc
@@ -2,8 +2,24 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 vector<int> PlusOne(vector<int> A) {
-  // TODO - you fill in here.
-  return {};
+  if (A.empty()) {
+    return std::vector<int>{1};
+  }
+
+  int carry = 0;
+  for (auto iter = std::rbegin(A); iter != std::rend(A); iter = carry == 1 ? std::next(iter, 1) : std::rend(A)) {
+    if (*iter == 9) {
+      *iter = 0;
+      carry = 1;
+    } else {
+      ++(*iter);
+      carry = 0;
+    }
+  }
+  if (carry == 1) {
+    A.insert(std::begin(A), 1);
+  }
+  return A;
 }
 
 int main(int argc, char* argv[]) {
